2a567b1259b4339b6c95c139939a650672cf0140
Anastasia <example@ru>
2021-02-17 21:07:25 +0300

    Init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Modal, Home, Sidebar } from './components/index'
+
+
+
+const App = () => {
+	return (
+		<>
+			<Home />
+			<Modal />
+			<Sidebar />
+		</>
+	)
+}
+
+
+
+
+export default App
